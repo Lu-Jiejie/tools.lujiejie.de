9c7b239bb751de3ef9e714ca7d3422b2c9559a1d
Lu-Jiejie <lu-jiejie@outlook.com>
2026-05-22 21:13:39 +0800

    fix: improve transition effects and structure in BackToTop component

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -12,26 +12,28 @@ function scrollToTop() {
 
 <template>
   <Transition
-    enter-active-class="transition-opacity duration-200 ease-out"
+    enter-active-class="transition-opacity duration-150 ease-out"
     enter-from-class="op-0"
-    enter-to-class="op-65"
+    enter-to-class="op-100"
     leave-active-class="transition-opacity duration-150 ease-in"
-    leave-from-class="op-65"
+    leave-from-class="op-100"
     leave-to-class="op-0"
   >
-    <button
+    <div
       v-if="visible"
-      type="button"
-      title="Back to top"
-      aria-label="Back to top"
-
-      border="~ c-border"
-
-      text-lg rounded-xl bg-c-surface op-65 place-center size-10 panel-shadow transition-colors duration-150 bottom-4 right-4 fixed z-20
-      hover="op-90 border-c-border-strong"
-      @click="scrollToTop"
+      bottom-4 right-4 fixed z-20
     >
-      <span i-carbon-chevron-up op-60 />
-    </button>
+      <button
+        type="button"
+        title="Back to top"
+        aria-label="Back to top"
+        border="~ c-border"
+        text-lg rounded-xl bg-c-surface op-65 place-center size-10 panel-shadow transition-colors duration-150
+        hover="op-90 border-c-border-strong"
+        @click="scrollToTop"
+      >
+        <span i-carbon-chevron-up op-60 />
+      </button>
+    </div>
   </Transition>
 </template>
